80c7a6b13cec099d7cfca67a7122b11390e50a96
Nain <nainstudio0210@gmail.com>
2026-02-24 21:21:59 +0900

    Fix: Final security update and src fallback

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -118,7 +118,7 @@ export default function WorkPage() {
         span: "md:col-span-2 md:row-span-2",
         poster: "/work/IFC Seoul Nightscape_poster.jpg",
         galleryContents: [
-          { type: "youtube", youtubeId: "qMhh6JikfhU" },
+          { type: "youtube", youtubeId: "seOjwPZ9ZkE" },
           { type: "image", src: "/work/IFC Seoul Nightscape_poster.jpg" },
           { type: "image", src: "/work/IFC01_vp.jpg" }
         ]
