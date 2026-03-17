d22ac5e2ea49f775068cd6ae9386f2405daaadaa
Nain <nainstudio0210@gmail.com>
2026-03-17 18:57:30 +0900

    Apply Brick style UI and Mobile Swipe

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Page() {
               <motion.img
                 src="/logo02.png"
                 alt="NAIN"
-                className="w-40 md:w-64 h-auto object-contain"
+                className="w-33 md:w-50 h-auto object-contain"
                 initial={{ opacity: 0, scale: 0.95 }}
                 animate={{ opacity: 0.8, scale: 1 }}
                 transition={{ duration: 0.8 }}
