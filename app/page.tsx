a795b50ee180cedbc5807cad63f711786868105c
Nain <nainstudio0210@gmail.com>
2026-03-18 06:13:49 +0900

    Apply Brick style UI and Mobile Swipe

--- app/page.tsx
+++ app/page.tsx
@@ -98,7 +98,8 @@ export default function Page() {
           controls={false}
           controlsList="nodownload noplaybackrate"
         >
-          <source src="/background.mp4" type="video/mp4" />
+          {/* ★ 대표님이 주신 코드에서 이 부분 한 줄만 클라우디너리 영상 주소로 정확하게 바꿨습니다! ★ */}
+          <source src="https://res.cloudinary.com/dbgzgyk6j/video/upload/v1773781635/Showreel_Compile_01_Low_gas3sg.mp4" type="video/mp4" />
         </video>
       </section>
 
